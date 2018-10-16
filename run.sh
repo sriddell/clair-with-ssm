07e19b13cfcbdd6ef3a92e12e22aa32e7e435180
Shane Riddell <shane.riddell@ellucian.com>
2018-10-16 10:41:14 -0400

    Update to use explicit region

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-/get-ssm --parameter-name $CONFIG_PARAMETER_NAME
+AWS_DEFAULT_REGION=$CONFIG_PARAMETER_REGION /get-ssm --parameter-name $CONFIG_PARAMETER_NAME
 config_base64=$(/get-ssm --parameter-name $CONFIG_PARAMETER_NAME)
 echo $config_base64
 echo $config_base64 | base64 -d > /config/config.yml
