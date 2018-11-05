73f4d239814d715bca9ec1c389da7a3d1d2425cf
Shane Riddell <shane.riddell@ellucian.com>
2018-11-05 11:19:14 -0500

    Allow explicit config passing for testing

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
-config_base64=$(AWS_DEFAULT_REGION=$CONFIG_PARAMETER_REGION /get-ssm --parameter-name $CONFIG_PARAMETER_NAME)
+if [ -z "$config_base64" ]; then
+    config_base64=$(AWS_DEFAULT_REGION=$CONFIG_PARAMETER_REGION /get-ssm --parameter-name $CONFIG_PARAMETER_NAME)
+fi
 echo $config_base64 | base64 -d > /config/config.yml
 /clair --config /config/config.yml
